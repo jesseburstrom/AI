d281101aea6b6f3c254153c8bd2882d792dada47
Jesse Burström <jesse@prevas.se>
2025-04-08 15:28:02 +0200

    fixes

--- ai_assistant/vite.config.ts
+++ ai_assistant/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/ai/',
+  base: '/assistant/',
   // build: {
   //   outDir: 'ai/dist', // 👈 output directory
   // },
